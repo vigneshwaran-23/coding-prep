7099fa77dd9203e41cbd0202a77e43ec6369b2d0
vigneshwaran-23 <vigneshapr23@gmail.com>
2024-01-14 00:15:26 +0530

    looping

--- looping.c
+++ looping.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
+int main()
+{
+    printf("---------------\n");
+    printf("Using for loop:\n");
+    for(int i=1; i<=5; i++)
+    printf("%d \n",i);
+    printf("---------------\n");
+    printf("Using while loop:\n");
+    int j=1;
+    while(j<=6){
+    printf("%d\n",j);
+    j++;}
+    printf("---------------\n");
+    printf("Using do-while loop:\n");
+    int k=1;
+    do
+    {
+        printf("%d\n",k);
+        k++;
+    } while (k<=7);
+    printf("---------------\n");
+    
+
+
+    
+    
+
+
+}
+
+
 
