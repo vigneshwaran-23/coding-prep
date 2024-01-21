c994e5324e421a772483f7b3ec0db49e5c207eb3
Vigneshwaran S <109017593+vigneshwaran-23@users.noreply.github.com>
2024-01-21 23:57:56 +0530

    Create selectionsort.c

---
+++ selectionsort.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j, min_idx;
+
+    // Input the number of elements in the array
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Input the elements of the array
+    printf("Enter the elements:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Selection sort algorithm
+    for (i = 0; i < n-1; i++) {
+        min_idx = i;
+        for (j = i+1; j < n; j++) {
+            if (arr[j] < arr[min_idx]) {
+                min_idx = j;
+            }
+        }
+
+        // Swap the found minimum element with the first element
+        int temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
+    }
+
+    // Print the sorted array
+    printf("Sorted array:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
